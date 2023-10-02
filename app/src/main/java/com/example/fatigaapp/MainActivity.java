5cdc8c65f10206703916976603cb91fbd15a726c
victorvillota27 <victorvillota27@gmail.com>
2023-10-02 18:55:35 -0500

    [ADD] se agrega la libreria openCV 4.7

--- app/src/main/java/com/example/fatigaapp/MainActivity.java
+++ app/src/main/java/com/example/fatigaapp/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.fatigaapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import org.opencv.android.OpenCVLoader;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if(OpenCVLoader.initDebug()) {
+            Log.d("LOADED","success");
+        }
+        else {
+            Log.d("LOADED","error de carga");
+        }
     }
 }
