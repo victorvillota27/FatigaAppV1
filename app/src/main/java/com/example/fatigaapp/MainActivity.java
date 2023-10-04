0e15d3f698c44c4392306e3284b839cbf3f24097
victorvillota27 <victorvillota27@gmail.com>
2023-10-03 19:34:17 -0500

    [UPDATE] cambio verion de java. se agrega funcionalidad para uso de camara

--- app/src/main/java/com/example/fatigaapp/MainActivity.java
+++ app/src/main/java/com/example/fatigaapp/MainActivity.java
@@ -1,24 +1,80 @@
 package com.example.fatigaapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.util.Log;
 
+import org.opencv.android.CameraActivity;
+import org.opencv.android.CameraBridgeViewBase;
 import org.opencv.android.OpenCVLoader;
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Collections;
+import java.util.List;
+
+public class MainActivity extends CameraActivity {
+
+    CameraBridgeViewBase cameraBridgeViewBase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        getPermission();
+        cameraBridgeViewBase = findViewById(R.id.cameraView);
+        cameraBridgeViewBase.setCameraIndex(1);
+
+        cameraBridgeViewBase.setCvCameraViewListener(new CameraBridgeViewBase.CvCameraViewListener2() {
+            @Override
+            public void onCameraViewStarted(int width, int height) {
+
+            }
+
+            @Override
+            public void onCameraViewStopped() {
+
+            }
+
+            @Override
+            public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
+                Mat frame = inputFrame.rgba();
+                Mat mirroredFrame = new Mat();
+                Core.flip(frame, mirroredFrame, 1);
+                return mirroredFrame;
+            }
+        });
         if(OpenCVLoader.initDebug()) {
             Log.d("LOADED","success");
+            cameraBridgeViewBase.enableView();
         }
         else {
-            Log.d("LOADED","error de carga");
+            Log.d("LOADED","error");
         }
     }
+    void getPermission(){
+        if(checkSelfPermission(android.Manifest.permission.CAMERA)!= PackageManager.PERMISSION_GRANTED){
+            requestPermissions(new String[]{Manifest.permission.CAMERA},101);
+
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(grantResults.length>0 && grantResults[0]!= PackageManager.PERMISSION_GRANTED){
+            getPermission();
+        }
+    }
+
+    @Override
+    protected List<? extends CameraBridgeViewBase> getCameraViewList() {
+        return Collections.singletonList(cameraBridgeViewBase);
+    }
+
 }
